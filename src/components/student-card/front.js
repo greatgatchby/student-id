c2c24c44b0d977d5bb43dfb1663d34c1866fa89d
greatgatchby <40676529+greatgatchby@users.noreply.github.com>
2023-02-28 15:42:25 +0000

    initial commit

--- src/components/student-card/front.js
+++ src/components/student-card/front.js
@@ -7,9 +7,7 @@ const Front = ({student}) => {
                 Lorum University
             </div>
             <p>id: {student.studentID}</p>
-            <div className={'avatar-container'}>
                 <img src={student.picture} alt={'student id'} className={'avatar'} />
-            </div>
             <div className={'card-content text-start p-2'} style={{fontSize: '1.2rem'}}>
                 <strong>Name: </strong>{student.name}<br/>
                 <strong>Department: </strong>{student.department}<br/>
